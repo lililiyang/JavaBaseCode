7d214780ba35507413fe5dfd6237d1df39ea81b2
liyang <1305845247@qq.com>
2019-05-21 20:04:20 +0800

    学习注解

--- src/ly/basetest/annotation/MyAnnotation.java
+++ src/ly/basetest/annotation/MyAnnotation.java
@@ -5,6 +5,8 @@ import java.lang.annotation.RetentionPolicy;
 
 /**
  * 自定义注解
+ * @Retention只能用于修饰其他的Annotation，用于指定被修饰的Annotation被保留多长时间。
+ * @Retention默认是class，而反射是在运行时期来获取信息的。因此就获取不到Annotation的信息了。于是，就得在自定义注解上修改它的RetentionPolicy值
  */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface MyAnnotation {
